7a2014ef527396132d50c3bf54217795d9df940f
nrhuizar <nrhuizar@gmail.com>
2021-01-10 10:21:27 -0800

    added express routes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const { notes } = require('./data/db.json');
+const apiRoutes = require('./public/routes/apiRoutes');
+const htmlRoutes = require('./public/routes/htmlRoutes');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on PORT ${PORT}`);
+});
